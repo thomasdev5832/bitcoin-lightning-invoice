43ae2b2e4917262d9c87aa17712e5817fd168513
thomasdev5832 <eugabrielthome@gmail.com>
2025-07-07 12:21:06 -0300

    fix(frontend): improve header logo resolution

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,7 +3,6 @@ import { FiZap, FiCopy, FiRefreshCw, FiLoader, FiCheck, FiClock, FiGlobe, FiMenu
 import { MdCurrencyBitcoin } from "react-icons/md";
 import { QRCodeSVG } from "qrcode.react";
 import { useNavigate } from "react-router-dom";
-import LogoLightningOrangeNoBg from "../assets/img/NWC-BIG-Orange-NOBG.svg";
 import LogoLightningWhite from "../assets/img/NWC-Logo-Lightning-White-No-bg.svg";
 import LogoLightningOrange from "../assets/img/NWC-Logo-Lightning-Orange-No-bg.svg"
 import NWCWallet from "../assets/img/NWCPay-wallet.png";
@@ -119,8 +118,18 @@ const LandingPage = () => {
             <nav className="bg-white w-full z-50">
                 <div className="container mx-auto px-6 py-4">
                     <div className="flex items-center justify-between">
-                        <div className="flex items-center">
-                            <img src={LogoLightningOrangeNoBg} className="h-10" alt="NWCPay Logo" />
+                        <div className="flex flex-row items-center px-3 rounded-sm gap-1">
+                            <div className="bg-orange-500 flex items-center justify-center rounded-md p-1">
+                                <svg className="w-5 h-5" viewBox="0 0 80.76923076923077 116.82287105705548">
+                                    <g transform="translate(-18.144373456698258, -0.27794033690228215) scale(1.1727437992501784)"
+                                        fill="#fff">
+                                        <path
+                                            d="M84.228,4.966c-0.296-1.263-1.122-2.275-2.33-2.851l-2.64-1.257c-0.155-0.291-0.52-0.621-1.378-0.621  c-0.351,0-0.756,0.056-1.204,0.165L50.715,6.727c-2.497,0.609-5.491,2.96-6.675,5.242L24.903,48.86  c-0.569,1.096-0.642,2.097-0.205,2.815c0.476,0.783,1.518,1.136,2.856,0.954l3.169-0.426c1.15-0.153,2.051,0.13,2.455,0.771  c0.374,0.592,0.326,1.449-0.134,2.413l-17.013,35.97c-0.522,1.094-0.583,1.143-0.553,2.143H15.5v-0.543  c0,0.006-0.029,0.012-0.028,0.018l0.589,3.419c0.275,2.393,1.49,3.12,2.193,3.342c0.205,0.062,0.465,0.116,0.775,0.116  c0.763,0,1.804-0.321,2.933-1.606L65.73,48.434c1.145-1.302,1.639-2.887,1.355-4.351c-0.251-1.304-1.077-2.37-2.325-3.002  l-2.696-1.366c-0.189-0.384-0.669-0.824-1.884-0.824c-0.083,0-0.181,0.013-0.271,0.017L83.063,9.443  C84.146,8.065,84.57,6.434,84.228,4.966z M64.401,44.602c0.12,0.626-0.144,1.366-0.726,2.028L19.905,96.438  c-0.405,0.46-0.707,0.625-0.836,0.667c-0.083-0.108-0.238-0.415-0.308-1.024c0,0-0.606-3.586-0.552-4.913L61.14,42.312l2.384,1.208  C64.008,43.765,64.311,44.139,64.401,44.602z M80.912,7.754L56.106,39.323l-2.517,0.288c-1.12,0.128-1.918-0.099-2.168-0.612  c-0.236-0.488,0.005-1.248,0.661-2.084L78.368,3.464l2.354,1.121c0.453,0.216,0.737,0.555,0.844,1.005  C81.718,6.244,81.48,7.032,80.912,7.754z"
+                                        />
+                                    </g>
+                                </svg>
+                            </div>
+                            <h3 className="text-orange-500 font-anton text-xl pr-1">NWCPay</h3>
                         </div>
 
                         <div className="hidden md:flex text-orange-500 items-center space-x-8 font-semibold">
@@ -456,12 +465,12 @@ const LandingPage = () => {
                             <div className="flex flex-col items-center bg-zinc-900 backdrop-blur-sm p-6 sm:p-10 lg:p-12 rounded-2xl shadow-xl border border-gray-700/50 animate-fade-in">
                                 <div className="flex flex-col sm:flex-row items-center gap-4 mb-6">
                                     <GiLightningShield
-                                        className="text-orange-500 w-10 h-10 sm:w-12 sm:h-12 transform transition-transform duration-300 hover:scale-110"
+                                        className="text-orange-500 w-10 h-10 transform transition-transform duration-300 hover:scale-110"
                                         aria-hidden="true"
                                     />
                                     <h2
                                         id="security-title"
-                                        className="text-xl sm:text-4xl lg:text-5xl font-extrabold text-white tracking-tight"
+                                        className="text-xl sm:text-3xl font-extrabold text-white tracking-tight"
                                     >
                                         100% Non-Custodial Security
                                     </h2>
@@ -486,12 +495,12 @@ const LandingPage = () => {
                             <div className="flex flex-col items-center bg-zinc-900/50 backdrop-blur-sm p-6 sm:p-10 lg:p-12 rounded-2xl shadow-xl border border-gray-700/50 animate-fade-in">
                                 <div className="flex flex-col sm:flex-row items-center gap-4 mb-6">
                                     <FiClock
-                                        className="text-orange-500 w-10 h-10 sm:w-12 sm:h-12 transform transition-transform duration-300 hover:scale-110"
+                                        className="text-orange-500 w-10 h-10 transform transition-transform duration-300 hover:scale-110"
                                         aria-hidden="true"
                                     />
                                     <h2
                                         id="coming-soon-title"
-                                        className="text-xl sm:text-4xl lg:text-5xl font-extrabold text-white tracking-tight"
+                                        className="text-xl sm:text-3xl font-extrabold text-white tracking-tight"
                                     >
                                         Exciting Features Coming Soon
                                     </h2>
@@ -619,12 +628,11 @@ const LandingPage = () => {
             <section className="py-20 bg-white">
                 <div className="container mx-auto px-6 text-center flex flex-col items-center">
                     <div className="flex items-center flex-col">
-                        <img src={LogoLightningOrange} className="h-20" alt="NWCPay Logo" />
+                        <img src={LogoLightningOrange} className="h-15" alt="NWCPay Logo" />
                         <h2 className="text-4xl font-black mb-6 text-orange-500">Try NWCPay Now</h2>
                     </div>
                     <p className="text-xl text-zinc-700 font-medium mb-8 max-w-2xl mx-auto">
-                        Connect your Nostr Wallet Connect-compatible wallet and generate your first Lightning invoice.
-                        <br /> Start accepting Bitcoin in seconds, anywhere in the world.
+                        Connect your wallet and start accepting Bitcoin in seconds.
                     </p>
                     <button
                         onClick={() => navigate("/connect")}
